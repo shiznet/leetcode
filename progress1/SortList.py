8a4d6b1f82a16aaaea0f02407096e6a7f559bb51
newcomer <shiznet3908@gmail.com>
2014-07-10 00:21:21 +0800

    sort node list with merge sorting

--- progress1/SortList.py
+++ progress1/SortList.py
@@ -1,4 +1,4 @@
-
+#-*- coding: ASCII  -*-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -66,22 +66,44 @@ class Solution:
         nodeBpre.next.next = nodeAnext
         
 
-    def merge(self, nodeAPre, nodeBPre, length):
+    def merge(self, nodeAPre = None, nodeBPre = None, length = 0):
         pointB = 0
-        nAPre = nodeApre
-        nBPre = nodeBpre
+        nAPre = nodeAPre
+        nBPre = nodeBPre
         while True:
-            if nBpre.next is not None | pointB<length :
+            if nBPre.next is not None or pointB<length :
                 break
-            if nApre.next.val<=nBpre.next.val:
-                prepend(nApre,nBpre)
+            if nAPre.next.val<=nBPre.next.val:
+                prepend(nAPre,nBPre)
             else:
-                append(nApre,nBpre)
-            nApre = nApre.next
+                append(nAPre,nBPre)
+            nAPre = nAPre.next
             pointB+=1
             
             
+    def merge(self,node):
+        loop = 1
+        step = 1
+        while True:
+            nOf1Seg = node
+            nOf2Seg = self.getStartNodeOfSecSeg(nOf1Seg,step)
+            
+
+    def getStartNodeOfSecSeg(self,node,length):
+        pointer = node
+        while length>0:
+            length-=1
+            if pointer is not None :
+                pointer = pointer.next
+            else:
+                break
+        return pointer
+
+
     def iterateNode(self,node):
+        '''
+        Just for debug.
+        '''
         point = node
         while True:
             if point is not None:
@@ -95,6 +117,17 @@ class NodeList:
         self.val=value
         self.next=None
 
+def getstarOfNextSeg(node,length):
+    s = Solution()
+    a = NodeList(1)
+    b = NodeList(2)
+    c = NodeList(3)
+    d = NodeList(4)
+    a.next = b
+    b.next = c
+    c.next = d
+    secNode = s.getStartNodeOfSecSeg(a,2)
+    print secNode.val
 if __name__ == '__main__':
     s = Solution()
     a = NodeList(1)
@@ -104,11 +137,12 @@ if __name__ == '__main__':
     a.next = c
     c.next = b
     b.next = d
-    s.iterateNode(a)
-    head = NodeList(0)
-    head.next = a
-    s.merge(head,b,2)
-    s.iterateNode(a)
+    getstarOfNextSeg(a,2)
+    #s.iterateNode(a)
+    #head = NodeList(0)
+    #head.next = a
+    #s.merge(head,b,2)
+    #s.iterateNode(a)
     #s.append(head,c)
     #s.iterateNode(a)
     #s.prepend(head,d)
